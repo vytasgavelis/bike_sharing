c3334726140661b9e5a8293268daa30065ca0415
Vytas <gavelisvytas@gmail.com>
2022-03-12 17:38:31 +0200

    only allow admin to download qr code

--- station_admin/urls.py
+++ station_admin/urls.py
@@ -7,11 +7,12 @@ from .views import IndexView
 #from .views.client import ParkingSiteListView
 from .views.client.parking import ParkingSiteListView
 from .views.admin.qr_code_download_view import QrCodeDownloadView
+from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('register', RegistrationView.as_view(), name='register'),
     path('parking-site', ParkingSiteListView.as_view(), name='parking_site_list'),
     #todo: add following to admin urls.
-    path('qr-code/<int:qr_code_id>/', QrCodeDownloadView.as_view(), name='qr_code_download'),
+    path('qr-code/<int:qr_code_id>/', staff_member_required(QrCodeDownloadView.as_view()), name='qr_code_download'),
 ]
--- station_admin/views/admin/qr_code_download_view.py
+++ station_admin/views/admin/qr_code_download_view.py
@@ -1,9 +1,7 @@
 from django.views import View
 from django.http import HttpResponse
 
-
 class QrCodeDownloadView(View):
-    # TODO: add authentication
     def get(self, *args, **kwargs) -> HttpResponse:
         id = kwargs['qr_code_id']
         return HttpResponse('downloading qr code!')
